72029ec96f0be6ead25b85d925c2eaf1a61b7d97
ffx@X64 <ffx@x64>
2016-09-20 20:07:56 +0200

    Updated

--- backup.sh
+++ backup.sh
@@ -41,75 +41,38 @@ esac
 #Define function to backup Needed files
 start_backup() {
 
-case $hostname in
-	SFX*)
-	tar cjf $i/Backup/$hostname/$DATE.OPENSVC.tbz /opt/opensvc/etc/* >/dev/null 2>&1
-	tar cjf $i/Backup/$hostname/$DATE.IFCFG.tbz /etc/sysconfig/network/* >/dev/null 2>&1
-	tar cjf $i/Backup/$hostname/$DATE.SCRIPTS.tbz /opt/ffx/scripts/* >/dev/null 2>&1
-	;;
+echo "#### Starting Backup $TIME ####" >> $LOG_FILE
+tar cjf $i/Backup/$hostname/$DATE.SCRIPTS.tbz /opt/ffx/docker/ /opt/ffx/systems /opt/ffx/scripts /opt/ffx/centreon >/dev/null 2>&1
+tar cjf $i/Backup/$hostname/$DATE.ETC.tbz /etc/fstab /etc/network/interfaces /etc/systemd/system/ /etc/default/docker >/dev/null 2>&1
+echo "#### End Backup $TIME ####" >> $LOG_FILE && echo "" >> $LOG_FILE
+echo "----------------------" >> $LOG_FILE && echo "" >> $LOG_FILE
 
-	OSX|MAC*)
-	tar cjf $i/Backup/$hostname/$DATE.CONF.tbz  /etc/nrpe.cfg /etc/snmp/snmpd.conf ~/.bashrc /etc/hosts >/dev/null 2>&1
-	tar cjf $i/Backup/$hostname/$DATE.PLUGINS.tbz  /usr/lib/nagios/plugins/ >/dev/null 2>&1
-	tar cjf $i/Backup/$hostname/$DATE.SCRIPTS.tbz /opt/ffx/scripts/* >/dev/null 2>&1
-	;;
+}
 
-	*)
-	tar cjf $i/Backup/$hostname/$DATE.SCRIPTS.tbz /opt/ffx/docker/ >/dev/null 2>&1
-	tar cjf $i/Backup/$hostname/$DATE.ETC.tbz /etc/fstab /etc/network/interfaces /etc/systemd/system/ >/dev/null 2>&1
-	;;
+purge_backup() {
+echo "#### Starting Purge $TIME ####" >> $LOG_FILE
+find $i/Backup/ -type f -iname *.tbz -mtime +$RETENTION -delete && echo "Purge was successful in $i" >> $LOG_FILE
+echo "#### End Purge $TIME ####" >> $LOG_FILE && echo "" >> $LOG_FILE
+echo "----------------------" >> $LOG_FILE && echo "" >> $LOG_FILE
+}
 
-	*)
-	echo "Unknown host, add a New entry in $0."
-	return 1
-	;;
-esac
+check_mountpoint() {
+[[ `mountpoint -q $i` ]] && return 0 || return 1
 }
 
 case $hostname in
 	OSX|MAC*)
 		start_backup
 		if [ $? -eq 0 ] ; then
-			find $i/Backup/ -type f -iname *.tbz -mtime +$RETENTION -delete && echo "Purge was successful in $i" >> $LOG_FILE
+			purge_backup
 		fi
 	;;
 
 	*)
 		for i in $TARGET_DIR ; do
-			if mountpoint -q $i ; then
-				echo "Mount Point $i found, starting backup ..." >> $LOG_FILE
-				echo "#### Starting Backup $TIME ####" >> $LOG_FILE
-				start_backup
-				if [ $? -eq 0 ] ; then
-					echo "Starting Purge in $i" >> $LOG_FILE
-					find $i/Backup/ -type f -iname *.tbz -mtime +$RETENTION -delete && echo "Purge was successful in $i" >> $LOG_FILE
-				fi
-				echo "#### End $TIME ####" >> $LOG_FILE
-				echo "" >> $LOG_FILE
-				echo "----------------------" >> $LOG_FILE
-				echo "" >> $LOG_FILE
-			else
-				echo "!!!!!!! Mountpoint $i not found, trying to fix ... !!!!!!!" >> $LOG_FILE
-				umount $i && echo ">>> Moutpoint $i successfully unmounted. <<<" >> $LOG_FILE
-				mount $i && echo ">>> Moutpoint $i successfully remounted.<<<" >> $LOG_FILE || echo ">>> Moutpoint $i failed to remount after attempt. <<<" >> $LOG_FILE 
-
-				if mountpoint -q $i ; then
-					echo ">>> Mountpoint $i found, after a retry attempt, starting backup ... <<<" >> $LOG_FILE
-					start_backup
-
-					if [ $? -eq 0 ] ; then
-						echo "Starting Purge in $i" >> $LOG_FILE
-						find $i/Backup/ -type f -iname *.tbz -mtime +$RETENTION -delete && echo "Purge in $i done." >> $LOG_FILE
-					fi
-
-				else
-					echo "Problem on $i backup, please check mount options." >> $LOG_FILE
-				fi
-				echo "#### End $TIME ####" >> $LOG_FILE
-				echo "" >> $LOG_FILE
-				echo "----------------------" >> $LOG_FILE
-				echo "" >> $LOG_FILE
-			fi
+			check_mountpoint && start_backup && purge_backup || echo "#### Problem on folder $i during at $TIME ####" >> $LOG_FILE
+			umount $i ; mount $i
+			check_mountpoint && start_backup || echo "Problem on $i mountpoint, please check mount options." >> $LOG_FILE
 		done
 	;;
 esac
