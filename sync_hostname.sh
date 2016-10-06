532bcd7d4966d68c4dd58d38d962a1cf2905cdcb
ubuntumx <ffx@ubuntumx>
2016-10-06 20:22:55 +0200

    Updated for ubuntumx

--- sync_hostname.sh
+++ sync_hostname.sh
@@ -6,3 +6,7 @@ HOSTNAME=$(hostname | tr '[:lower:]' '[:upper:]')
 cp $WORKING_DIR/hosts /etc/
 sed -i '${/^127.0.0.1/d;}' /etc/hosts
 echo "127.0.0.1       $HOSTNAME" >> /etc/hosts
+
+if [ $(uname -n) = "UBUNTUMX" ]; then
+	sed -i '${/^192.168.56/d;}' /etc/hosts
+fi
