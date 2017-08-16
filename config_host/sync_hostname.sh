fbe26d29c8805a020e1ca604cca32187b09dfddb
sebastianpetrovich@gmail.com <root@SFX11.(none)>
2017-08-16 11:31:18 +0100

    x

--- config_host/sync_hostname.sh
+++ config_host/sync_hostname.sh
@@ -3,10 +3,17 @@
 WORKING_DIR="$( cd "$( dirname $0)" && pwd )"
 HOSTNAME=$(hostname | tr '[:lower:]' '[:upper:]')
 
-cp $WORKING_DIR/hosts /etc/
-sed -i '${/^127.0.0.1/d;}' /etc/hosts
-echo "127.0.0.1       $HOSTNAME" >> /etc/hosts
+case `uname -s` in
+        Linux)
+		SED="sed" ;;
+	SunOS)
+		SED="gsed" ;;
+	*)
+		echo "This script is only suitable for Linux or Solaris"
+	;;
+esac
 
-if [ $(uname -n) = "UBUNTUMX" ]; then
-	sed -i '/^192.168.56/d' /etc/hosts
-fi
+
+cp $WORKING_DIR/hosts /etc/
+$SED -i '${/^127.0.0.1/d;}' /etc/hosts
+echo "127.0.0.1       $HOSTNAME" >> /etc/hosts
