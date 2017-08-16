6c3834bdb97c5e79bcd296074382d79e6f400f85
sebastianpetrovich@gmail.com <root@SFX11.(none)>
2017-08-16 19:43:50 +0100

    Git basic configuration

---
+++ git/git.sh
@@ -0,0 +1,5 @@
+git config --global http.proxy http://proxy:8080/
+git config --global https.proxy http://proxy:8080/
+git config --global credential.helper store
+git config --global user.name "$HOSTNAME"
+git config --global user.mail "horus@gmail.com"
