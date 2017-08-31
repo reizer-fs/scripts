d8bd8e7e608446b9489ca9de2c19cc33ecd7a8f9
reizer-fs <sebastianpetrovich@gmail.com>
2017-08-31 19:11:53 +0100

    Fixed ssh copy

--- ssh/ssh_copy.sh
+++ ssh/ssh_copy.sh
@@ -10,11 +10,14 @@ if [ ! -z $1 ]; then
 	HOSTS=$1
 fi
 
-
 for hosts in $HOSTS ; do
 	if ping -c 1 $hosts >/dev/null 2>&1 ; then
 		echo "Processing $hosts ..."
-		ssh-copy-id -i $HOME/.ssh/id_rsa.pub root@$hosts >/dev/null 2>&1
+		if [ `type -p ssh-copy-id` ]; then
+			ssh-copy-id -i $HOME/.ssh/id_rsa.pub root@$hosts >/dev/null 2>&1 && echo "Key copied"
+		else
+			cat $HOME/.ssh/id_rsa.pub | ssh $hosts 'cat >> .ssh/authorized_keys && echo "Key copied"'
+		fi
 	else
 		echo "No route to $hosts."
 	fi
