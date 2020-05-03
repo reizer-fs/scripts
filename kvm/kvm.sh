4a934f0a555c3192f6722bb4b2d589425d068786
Sebastian Petrovich <sebastianpetrovich@gmail.com>
2020-05-04 01:45:46 +0200

    feat added new script and multiple fix

--- kvm/kvm.sh
+++ kvm/kvm.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-set -e
+
+# Exit immediately if a command exits with a non-zero status.
+#set -e
 
 # Set program name variable - basename without subshell
 prog=${0##*/}
 
-function usage ()
-{
-	    cat << EOF
+usage () {
+    cat << EOF
 NAME
     kvm-install-vm - Install virtual guests using cloud-init on a local KVM
     hypervisor.
@@ -22,22 +23,36 @@ DESCRIPTION
 COMMANDS
     help        - show this help or help for a subcommand
     attach-disk - create and attach a disk device to guest domain
-    create      - create a new guest domain
     detach-disk - detach a disk device from a guest domain
     list        - list all domains, running and stopped
-    remove      - delete a guest domain
 
 -h|--help           Print help and exit.
+-c|--create         Create a new guest domain
+-r|--remove         Remove a guest domain
+-v|--verbose        Enable debugging kernel options.
+-n|--name           Define the hostname foe the guest system."
 -d|--dry-run        Print only throughput, do not apply command.
 -l|--legacy         Use files from repository (require Internet connection).
 -o|--os             Select the operating system to install.
--n|--name           Define the hostname foe the guest system."
+-t|--text           Start the guest in text mode
+-k|--kickstart      Enable kickstart installation mode
 
 EOF
 exit 0
 }
 
-OPTS=`getopt -o vhn:o:dl --long verbose,help,name:,os:,dry-run,legacy -n 'parse-options' -- "$@"`
+delete_domain () {
+    [[ -z $1 ]] && echo "Argument required" && return 1
+    HOST=$1
+    # Stop and undefine the VM
+    #virsh list --name | grep -q $HOST && sudo virsh destroy $HOST &>/dev/null
+    #virsh list --all --name | grep -q $HOST && sudo virsh undefine $HOST --remove-all-storage &>/dev/null
+    sudo virsh destroy $HOST &>/dev/null
+    sudo virsh undefine $HOST --remove-all-storage &>/dev/null
+    sudo virsh pool-destroy $HOST &>/dev/null
+}
+
+OPTS=`getopt -o vhn:o:dltkrc --long verbose,help,name:,os:,dry-run,legacy,text,kickstart,create,remove -n 'parse-options' -- "$@"`
 
 if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
 
@@ -46,12 +61,16 @@ eval set -- "$OPTS"
 
 while true; do
   case ${1} in
-      -v|--verbose)     VERBOSE=true; shift;;
-      -h|--help)        HELP=true ; shift ;;
-      -d|--dry-run)     DRY_RUN=true; shift ;;
-      -l|--legacy)      LEGACY=true; shift ;;
-      -n|--name)        HOST=$2 ; shift ; shift ;;
-      -o|--os )         TEMPLATE=$2 ; shift ; shift ;;
+      -c|--create)      CREATE="true" ;     shift ;;
+      -r|--remove)      DELETE="true" ;     shift ;;
+      -v|--verbose)     VERBOSE="true" ;    shift ;;
+      -h|--help)        HELP="true" ;       shift ;;
+      -d|--dry-run)     DRY_RUN="true" ;    shift ;;
+      -l|--legacy)      LEGACY="true" ;     shift ;;
+      -t|--text )       TEXT="true" ;       shift ;;
+      -k|--kickstart )  KS="true" ;         shift ;;
+      -n|--name)        HOST=$2 ;           shift ; shift ;;
+      -o|--os )         TEMPLATE=$2 ;       shift ; shift ;;
       --) shift ; break ;;
       *) break ;;
   esac
@@ -59,36 +78,57 @@ done
 
 DIR_BASE="$HOME/VMs"
 DIR_HOST="${DIR_BASE}/${HOST}"
-[[ "$HELP" == "true" ]] &&  usage && exit 0
-[[ -z "$HOST" ]] && echo "[ error ] : Invalid name or no name specified." && exit 1
-[[ -z "$HOST" ]] && echo "[ error ] : Invalid name or no name specified." && usage && exit 1
-[[ -z "$TEMPLATE" ]] && echo "[ error ] :  Operating system not specified." && usage && exit 1
-[[ ! -d "$DIR_BASE" ]] && echo "[ error ] : ${DIR_BASE} not found." && exit 1
+KICKSTART_CFG="$HOME/Git/kickstart/ks_rhl8_minimal.cfg"
+KICKSTART_URL_CFG="http://10.11.12.1/ks_rhl8_minimal.cfg"
+EXTRA_ARGS="$EXTRA_ARGS -x 'ipv6.disable=1' -x 'ip=dhcp'"
+OSVARIANT="Linux"
 
-if [ "$LEGACY" == "true" ]; then
+[[ "$HELP" == "true" ]] &&  usage && exit 0
+[[ -z "$HOST" ]] && echo "[ error ] : Invalid name or no name specified." && usage && exit 1
+[[ -z "$TEMPLATE" && $CREATE == "true" ]] && echo "[ error ] :  Operating system not specified." && usage && exit 1
+[[ ! -d "$DIR_BASE" ]] && mkdir $DIR_BASE
+[[ ! -d "$DIR_HOST" ]] && mkdir $DIR_HOST
+
+[[ "$LEGACY" == "true"  && $CREATE != '' ]] &&
+{
     case $TEMPLATE in
         debian8)     LOCATION='http://ftp.nl.debian.org/debian/dists/jessie/main/installer-amd64/' ;;    
-        debian7)     LOCATION='http://ftp.nl.debian.org/debian/dists/wheezy/main/installer-amd64/' ;;    
         kali)     LOCATION='http://http.kali.org/kali/dists/kali-rolling/main/installer-amd64/' ;;    
-        centos8)     LOCATION='http://centos.mirrors.proxad.net/8/BaseOS/x86_64/os/' ;;    
+        #centos8)     LOCATION='http://centos.mirrors.proxad.net/8/BaseOS/x86_64/os/' ;;    
         centos8)     LOCATION='http://mirror.centos.org/centos/8/BaseOS/x86_64/kickstart/' ;;    
         centos7)     LOCATION='http://mirror.i3d.net/pub/centos/7/os/x86_64/' ;;    
-        centos6)     LOCATION='http://mirror.i3d.net/pub/centos/6/os/x86_64/' ;;    
         opensuse13)     LOCATION='http://download.opensuse.org/distribution/13.2/repo/oss/' ;;    
         opensuse12)     LOCATION='http://download.opensuse.org/distribution/12.3/repo/oss/' ;;    
         *)  echo "[ error ] : operating system not found. => $TEMPLATE" && exit 1 ;;
     esac
-    EXTRA_ARGS="--extra-args console=ttyS0,115200n8 --graphics none --location ${LOCATION} -x 'inst.ks=http://192.168.0.15:8000/ks_centos8.cfg'"
-fi
+    EXTRA_ARGS="$EXTRA_ARGS --location ${LOCATION}" ; }
 
-[[ -z "$DISK" ]] && DISK=" --disk path=${DIR_HOST}/${HOST}.qcow2,size=100,bus=virtio"
-[[ -z "$OSVARIANT" ]] && OSVARIANT="centos7.0"
+[[ "$LEGACY" == ''  && $CREATE == "true" ]] && 
+{
+    case $TEMPLATE in
+        win7)   	OSVARIANT="win7";		CDROM="$HOME/ISO/Windows7LITEX64.iso,device=cdrom,bus=ide --disk $HOME/ISO/virtio-win-drivers-20120712-1.iso,device=cdrom,bus=ide";;
+        centos8)	OSVARIANT="centos7.0";		CDROM="$HOME/OS/CentOS8/CentOS-8.1.1911-x86_64-boot.iso";;
+        solaris)
+                LOCATION="$HOME/ISO/sol-11_3-text-x86.iso,device=cdrom --graphics none "
+                DISK="  --disk path=${DIR_HOST}/${HOST}.qcow2,size=100,bus=ide"
+                OSVARIANT="solaris11 --noapic"
+                ;;
+        *)  echo "[ error ] : operating system not found. => $TEMPLATE" && exit 1 ;;
+    esac
+    EXTRA_ARGS="$EXTRA_ARGS --location $CDROM" ; }
+
 OSVARIANT="--os-variant $OSVARIANT"
-# Stop and undefine the VM
-sudo virsh list --name | grep -q $HOST && sudo virsh destroy $HOST &>/dev/null
-sudo virsh list --all --name | grep -q $HOST && sudo virsh undefine $HOST --storage ${DIR_HOST}/${HOST}.qcow2 &>/dev/null
-sudo rm -rf ${DIR_HOST} &>/dev/null ; mkdir ${DIR_HOST} &>/dev/null
-KVM="virt-install --name ${HOST} --ram 4096 --vcpus 2 --os-type linux --network bridge=br-docker ${OSVARIANT} ${DISK} ${EXTRA_ARGS}"
-if [ "$DRY_RUN" == 'true' ] ; then echo "sudo $KVM" ; exit ;fi
+[[ $KS == "true" ]] && EXTRA_ARGS="$EXTRA_ARGS -x inst.ks=$KICKSTART_URL_CFG"
+#[[ "$KS" == "true" ]] && EXTRA_ARGS="$EXTRA_ARGS --initrd-inject $KICKSTART_CFG -x 'ks=file:/ks.cfg'"
+[[ $VERBOSE == "true" ]] && EXTRA_ARGS="$EXTRA_ARGS --debug -x 'rd.shell' -x 'rd.debug'"
+[[ $TEXT == "true" ]] && EXTRA_ARGS="$EXTRA_ARGS -x console=ttyS0,115200n8 --nographics -x 'inst.text'"
+[[ -z $DISK ]] && DISK=" --disk path=${DIR_HOST}/${HOST}.qcow2,size=100,bus=virtio"
+[[ -z $OSVARIANT ]] && OSVARIANT="centos7.0"
+[[ $DELETE == "true" ]] && delete_domain $HOST
+
+KVM="virt-install --name ${HOST} --accelerate --ram 4096 --vcpus 1 --os-type linux --network bridge=br-kvm-docker ${OSVARIANT} ${DISK} ${EXTRA_ARGS}"
+[[ $DRY_RUN = 'true' ]] && echo "sudo $KVM" && exit 0
+[[ $CREATE = "true" ]] && { 
+delete_domain $HOST
 sudo qemu-img create -f qcow2 ${DIR_HOST}/${HOST}.qcow2 100G
-sudo ${KVM} && echo "Done."
+sudo ${KVM} ; }
